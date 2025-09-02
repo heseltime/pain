29745036653a3c1db05c33bd3bd396a0f59a77f4
Jack Heseltine <66922223+heseltime@users.noreply.github.com>
2025-09-02 15:16:43 +0200

    Add CORS

--- bumpserver/bumpserver/settings.py
+++ bumpserver/bumpserver/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'bump',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,6 +73,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bumpserver.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://p-a-i-n.world",
+    "https://p-a-i-n.world",  # add https variant if the site is served over TLS
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
